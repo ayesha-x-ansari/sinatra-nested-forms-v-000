dd264a68b84b389774a7265d2532ecd17363373b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-16 22:31:26 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor  :name, :weight, :height
+  @@pirate = []
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  #  pirates << self
+  end
+
+  def self.all
+    pirates
+  end
 end
